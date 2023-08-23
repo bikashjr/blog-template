29509c0e15bc0cad5c75337bc9f4a87a0ecc26c9
Bikash Ghimire <ghimirebikash41@gmail.com>
2023-08-23 17:15:16 +0545

    Initial Commit

---
+++ script.js
@@ -0,0 +1,130 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    // navText: ["<img src='./prev.png'>","<img src='./next.png'>"],
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
+});
+$(".stories-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  dots: true,
+  nav: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+      margin: 10,
+      stagePadding: 20,
+    },
+    600: {
+      items: 3,
+      margin: 20,
+      stagePadding: 50,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+$(".recommended-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  dots: true,
+  nav: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+      margin: 10,
+      stagePadding: 20,
+    },
+    600: {
+      items: 3,
+      margin: 20,
+      stagePadding: 50,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+    items: 4,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [979, 3],
+  });
+});
+$(document).ready(function () {
+  $("#news-slider").owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [980, 2],
+    itemsMobile: [600, 1],
+    navigation: true,
+    navigationText: ["", ""],
+    pagination: true,
+    autoPlay: true,
+  });
+});
+
+function scrollToTop() {
+  let mybutton = document.getElementById("myBtn");
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+}
+scrollToTop();
+
+
+
+/* <div class="container">
+<div class="row">
+  <div class="col-md-6">
+    <div class="copyright">
+      <i class="fa-regular fa-copyright mx-2"></i>
+      <p class="my-2">Copyright Traveller 2022</p>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="d-flex justify-content-end">
+      <img src="https://modtour.travelerwp.com/wp-content/uploads/2022/06/Frame-3182.svg" alt="">
+    </div>
+  </div>
+
+</div>
+</div> */
